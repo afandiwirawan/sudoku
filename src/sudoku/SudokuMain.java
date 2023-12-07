2f45d055039f330d3ca0d71cfde62d5f63df45d9
HajidRamadhan <147375143+HajidRamadhan@users.noreply.github.com>
2023-12-07 17:15:02 +0700

    Update SudokuMain.java
    playername

--- src/sudoku/SudokuMain.java
+++ src/sudoku/SudokuMain.java
@@ -1,7 +1,12 @@
 package sudoku;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
+/**
+ * The main Sudoku program
+ */
 public class SudokuMain extends JFrame {
     private static final long serialVersionUID = 1L;  // to prevent serial warning
 
@@ -9,28 +14,40 @@ public class SudokuMain extends JFrame {
     GameBoardPanel board = new GameBoardPanel();
     JButton btnNewGame = new JButton("New Game");
     JButton btnRestart = new JButton("Restart"); // New button for restart
+    JLabel timerLabel = new JLabel("Timer: 0 seconds");
+    String playerName;
+
+    // Timer variables
+    private Timer timer;
+    private int seconds;
 
     // Constructor
     public SudokuMain() {
+        // Prompt the user to enter their name
+        playerName = JOptionPane.showInputDialog(this, "Enter your name:");
+
         Container cp = getContentPane();
         cp.setLayout(new BorderLayout());
 
         cp.add(board, BorderLayout.CENTER);
 
-        // Add buttons to the south to re-start the game via board.newGame() and restart
+        // Add buttons and timer label to the south to re-start the game via board.newGame() and restart
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(btnNewGame);
         buttonPanel.add(btnRestart);
+        buttonPanel.add(timerLabel);
         cp.add(buttonPanel, BorderLayout.SOUTH);
 
         // Add ActionListener for New Game button
-        btnNewGame.addActionListener(e -> board.newGame());
+        btnNewGame.addActionListener(e -> startNewGame());
 
         // Add ActionListener for Restart button
         btnRestart.addActionListener(e -> restartGame());
 
-        // Initialize the game board to start the game
+        // Initialize the game board and timer
+        initializeTimer();
         board.newGame();
+        startTimer();
 
         pack();     // Pack the UI components, instead of using setSize()
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
@@ -38,10 +55,51 @@ public class SudokuMain extends JFrame {
         setVisible(true);
     }
 
+    // Method to start a new game
+    private void startNewGame() {
+        // Prompt the user to enter their name if not provided
+        if (playerName == null || playerName.trim().isEmpty()) {
+            playerName = JOptionPane.showInputDialog(this, "Enter your name:");
+        }
+
+        restartTimer();
+        board.newGame();
+    }
+
     // Method to restart the game
     private void restartGame() {
-        board.newGame(); // Call the newGame method on button click
+        restartTimer();
+        board.newGame();
         JOptionPane.showMessageDialog(this, "Game Restarted!");
+    }
+
+    // Method to initialize the timer
+    private void initializeTimer() {
+        seconds = 0;
+        timer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                seconds++;
+                updateTimerLabel();
+            }
+        });
+    }
+
+    // Method to start the timer
+    private void startTimer() {
+        timer.start();
+    }
+
+    // Method to restart the timer
+    private void restartTimer() {
+        timer.stop();
+        seconds = 0;
+        updateTimerLabel();
+        timer.start();
+    }
+
+    // Method to update the timer label
+    private void updateTimerLabel() {
+        timerLabel.setText("Timer: " + seconds + " seconds");
     }
 
     /** The entry main() entry method */
